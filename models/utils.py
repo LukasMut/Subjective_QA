edc9420238275fd0c9a807c2578e82ccc69769fa
LukasMut <lukas-mt@live.at>
2020-03-09 15:32:24 +0100

    "changes in test loop"

--- models/utils.py
+++ models/utils.py
@@ -868,7 +868,7 @@ def test(
           elif input_sequence == 'question_answer':
             b_input_ids, b_attn_masks, b_token_type_ids, b_input_lengths, b_sbj = batch
 
-        else:
+        else task == 'Domain_Classification':
           b_input_ids, b_attn_masks, b_token_type_ids, b_input_lengths, _, _,  _, b_domains = batch
 
         # if current batch_size is smaller than specified batch_size, skip batch (number of examples in last batche might not equal to batch_size)
