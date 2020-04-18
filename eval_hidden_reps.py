278bf3853d9d07ef1a70f25ac5dab0c1b886349b
LukasMut <lukas-mt@live.at>
2020-04-18 14:39:48 +0200

    Update eval_hidden_reps.py

--- eval_hidden_reps.py
+++ eval_hidden_reps.py
@@ -13,6 +13,7 @@ from eval_squad import compute_exact
 from scipy.spatial.distance import mahalanobis
 from scipy.stats import mode 
 from sklearn.decomposition import PCA
+from sklearn.metrics import f1_score
 
 
 def get_hidden_reps(source:str='SubjQA'):
@@ -59,7 +60,7 @@ def compute_ans_distances(
     for i, a_i in enumerate(a_hiddens):
         for j, a_j in enumerate(a_hiddens):
             #NOTE: we don't want to compute cosine sim of a vector with itself (i.e., cos_sim = 1),
-            #AND it's redundant to compute cosine sim (or Euclid dist) twice for some pair of vectors (i.e., cos_sim(u, v) == cos_sim(v, u))
+            #AND it's redundant to compute cosine sim (or Euclid dist) twice for some pair of vectors (i.e., cos_sim(u, v) == cos_sim(v, u) AND euclid(u, v) == euclid(v, u))
             if i != j and j > i:
                 if metric == 'cosine':
                     a_mean_dist += cosine_sim(u=a_i, v=a_j)
@@ -76,7 +77,6 @@ def evaluate_estimations(
                          test_results:dict,
                          metric:str,
                          dim:str,
-                         normalization:bool=False,
 ):
     pred_answers = test_results['predicted_answers']
     true_answers = test_results['true_answers']
@@ -94,23 +94,36 @@ def evaluate_estimations(
         pred_ans = pred_ans.strip()
         true_ans = true_answers[i].strip()
         #NOTE: for now we exclusively want to estimate model predictions w.r.t. answer spans that contain > 1 token
-        #if len(true_ans.split()) > 1:
-        if compute_exact(true_ans, pred_ans):
-            true_preds.append(1)
-            #pred_indices.append(i)
-        else:
-            #pass
-            true_preds.append(0)
-        pred_indices.append(i)
+        if len(true_ans.split()) > 1:
+            if compute_exact(true_ans, pred_ans):
+                true_preds.append(1)
+                #pred_indices.append(i)
+            else:
+                #pass
+                true_preds.append(0)
+            pred_indices.append(i)
     
     assert len(true_preds) == len(pred_indices)
+    pred_indices = np.array(pred_indices)
     true_preds = np.array(true_preds)
 
     print()
-    print(len(true_preds))
     print(Counter(true_preds))
     print()
 
+    # compute (dis-)similarities among hidden representations in H_a for both correct and erroneous model predictions at each transformer layer
+    ans_similarities, est_preds = compute_distances_across_layers(
+                                                       feat_reps=feat_reps,
+                                                       true_start_pos=true_start_pos,
+                                                       true_end_pos=true_end_pos,
+                                                       sent_pairs=sent_pairs,
+                                                       pred_indices=pred_indices,
+                                                       true_preds=true_preds,
+                                                       metric=metric,
+                                                       dim=dim,
+                                                      )
+
+    """
     # estimate model predictions w.r.t. hidden reps
     est_preds_top_layers = estimate_preds_wrt_hiddens(
                                                       feat_reps=feat_reps,
@@ -120,14 +133,87 @@ def evaluate_estimations(
                                                       pred_indices=pred_indices,
                                                       metric=metric,
                                                       dim=dim,
-                                                      normalization=normalization,
                                                       )
+    """
     est_accs = {}
-    est_accs['correct_preds'] = (true_preds[true_preds == 1] == est_preds_top_layers[true_preds == 1]).mean() * 100
-    est_accs['incorrect_preds'] = (true_preds[true_preds == 0] == est_preds_top_layers[true_preds == 0]).mean() * 100
-    est_accs['total_preds'] = (true_preds == est_preds_top_layers).mean() * 100
+    est_accs['correct_preds'] = (true_preds[true_preds == 1] == est_preds[true_preds == 1]).mean() * 100
+    est_accs['incorrect_preds'] = (true_preds[true_preds == 0] == est_preds[true_preds == 0]).mean() * 100
+    est_accs['total_preds'] = {} 
+    est_accs['total_preds']['acc'] = (true_preds == est_preds).mean() * 100
+    est_accs['total_preds']['f1_macro'] = f1_score(true_preds, est_preds, average='macro') * 100
     #est_accs = {'Layer' + '_' + str(l + 1): (est_preds == true_preds).mean() * 100 for l, est_preds in enumerate(est_preds_top_layers)}
-    return est_accs
+    return est_accs, ans_similarities
+
+def compute_distances_across_layers(
+                                    feat_reps:dict,
+                                    true_start_pos:list,
+                                    true_end_pos:list,
+                                    sent_pairs:list,
+                                    pred_indices:list,
+                                    true_preds:np.ndarray,
+                                    metric:str,
+                                    dim:str,
+                                    p_components:int=2,
+                                    rnd_state:int=42,
+                                    est_layer:int=5,
+                                    cos_thresh:int=.85,
+):
+    if dim == 'low':
+        pca = PCA(n_components=p_components, svd_solver='auto', random_state=rnd_state)
+
+    ans_similarities = defaultdict(dict)
+    N = len(pred_indices)
+    est_preds = np.zeros(N)
+
+    for l, hiddens_all_sents in feat_reps.items():
+        layer_no = int(l.lstrip('Layer' + '_'))
+        correct_preds_dists, incorrect_preds_dists = [], []
+        k = 0
+        for i, hiddens in enumerate(hiddens_all_sents):
+            if i in pred_indices:
+                sent = sent_pairs[i].split()
+                sep_idx = sent.index('[SEP]')
+                hiddens = np.array(hiddens)
+                
+                if dim == 'low':
+                    # transform feat reps into low-dim space with PCA
+                    hiddens = pca.fit_transform(hiddens)
+
+                # extract hidden reps for both answer and context 
+                a_hiddens = hiddens[true_start_pos[i]:true_end_pos[i]+1, :]
+                c_hiddens = np.vstack((hiddens[sep_idx+1:true_start_pos[i]], hiddens[true_end_pos[i]+1:-1,:]))
+
+                # compute mean hidden reps
+                a_mean_rep = a_hiddens.mean(0)
+                c_mean_rep = c_hiddens.mean(0)
+
+                a_mean_dist = compute_ans_distances(a_hiddens, metric)
+                a_c_dist = cosine_sim(u=c_mean_rep, v=a_mean_rep) if metric == 'cosine' else euclidean_dist(u=c_mean_rep, v=a_mean_rep)
+
+                if true_preds[pred_indices == i] == 1:
+                    correct_preds_dists.append((a_mean_dist, a_c_dist))
+                else:
+                    incorrect_preds_dists.append((a_mean_dist, a_c_dist))
+
+                # estimate model predictions w.r.t. avg cosine similarities in low-dim space among hidden reps in h_a AND dissimilarity to h_c in the penultimate transformer layer
+                if layer_no == est_layer:
+                    if a_mean_dist > cos_thresh:
+                        est_preds[k] += 1
+                k += 1
+
+        ans_similarities[l]['correct_preds'] = {}
+        ans_similarities[l]['correct_preds']['mean_cos_ha'] = np.mean(list(map(lambda d: d[0], correct_preds_dists)))
+        ans_similarities[l]['correct_preds']['std_cos_ha'] = np.std(list(map(lambda d: d[0], correct_preds_dists)))
+        ans_similarities[l]['correct_preds']['mean_cos_ha_hc'] = np.mean(list(map(lambda d: d[1], correct_preds_dists)))
+        ans_similarities[l]['correct_preds']['std_cos_ha_hc'] = np.std(list(map(lambda d: d[1], correct_preds_dists)))
+
+        ans_similarities[l]['incorrect_preds'] = {}
+        ans_similarities[l]['incorrect_preds']['mean_cos_ha'] = np.mean(list(map(lambda d: d[0], incorrect_preds_dists)))
+        ans_similarities[l]['incorrect_preds']['std_cos_ha'] = np.std(list(map(lambda d: d[0], incorrect_preds_dists)))
+        ans_similarities[l]['incorrect_preds']['mean_dist_ha_hc'] = np.mean(list(map(lambda d: d[1], incorrect_preds_dists)))
+        ans_similarities[l]['incorrect_preds']['std_dist_ha_hc'] = np.std(list(map(lambda d: d[1], incorrect_preds_dists)))
+
+    return ans_similarities, est_preds
 
 def estimate_preds_wrt_hiddens(
                                feat_reps:dict,
@@ -138,11 +224,9 @@ def estimate_preds_wrt_hiddens(
                                metric:str,
                                dim:str,
                                rnd_state:int=42,
-                               normalization:bool=False,
 ):  
     if dim == 'low':
-        #assert metric == 'euclid', 'Computing the cosine similarity between (word) vectors in low-dimensional (vector) space is not particularly useful. Thus, we must calculate the euclidean distance instead.'
-        pca = PCA(n_components=.99, svd_solver='auto', random_state=rnd_state) # initialise PCA
+        pca = PCA(n_components=2, svd_solver='auto', random_state=rnd_state) # initialise PCA
 
     est_preds_top_layers = []
     for l, hiddens_all_sent_pairs in feat_reps.items():
@@ -151,113 +235,42 @@ def estimate_preds_wrt_hiddens(
         if layer_no >= 4:
             N = len(pred_indices)
             est_preds_current_layer = np.zeros(N)
-            k = 0 # k = idx w.r.t. ans spans for which we want to estimate preds based on hidden reps
+            k = 0
             for i, hiddens in enumerate(hiddens_all_sent_pairs):
                 if i in pred_indices:
                     sent = sent_pairs[i].split()
                     sep_idx = sent.index('[SEP]')
                     hiddens = np.array(hiddens)
+                    # transform feat reps into low-dim space with PCA (only useful for computing Euclidean distances; not necessary for cosine or Mahalanobis)
+                    if dim == 'low':
+                        hiddens = pca.fit_transform(hiddens)
+
+                    a_indices = np.arange(true_start_pos[i], true_end_pos[i]+1)
                     q_hiddens = hiddens[1:sep_idx, :]
-                    
-                    if normalization or dim == 'low':
-                        # calculate mean and std of hidden rep matrix w.r.t. answer and context feat reps only
-                        hiddens = hiddens[sep_idx+1:-1, :]
-                        hiddens_mu = hiddens.mean(0)
-                        hiddens_sigma = hiddens.std(0)
-                        hiddens -= hiddens_mu # center features
-                        hiddens /= hiddens_sigma # normalize features
-
-                        # transform feat reps into low-dim space with PCA (only useful for computing Euclidean distances; not necessary for cosine or Mahalanobis)
-                        hiddens = pca.fit_transform(hiddens) if dim == 'low' else hiddens
-
-                        a_start = true_start_pos[i] - (sep_idx + 1)
-                        a_end = true_end_pos[i] - (sep_idx + 1)
-                        a_indices = np.arange(a_start, a_end + 1)
-                        c_hiddens = np.vstack((hiddens[:a_indices[0], :], hiddens[a_indices[-1]+1:, :]))
-                    
-                    else:
-                        a_indices = np.arange(true_start_pos[i], true_end_pos[i]+1)
-                        c_hiddens = np.vstack((hiddens[sep_idx+1:a_indices[0], :], hiddens[a_indices[-1]+1:-1, :]))
-
-
+                    c_hiddens = np.vstack((hiddens[sep_idx+1:a_indices[0], :], hiddens[a_indices[-1]+1:-1, :]))
                     a_hiddens = hiddens[a_indices, :]    
                     a_mean_rep = a_hiddens.mean(0)
 
                     if metric == 'cosine':
+                        
+                        cos_sims_a_and_c = np.array([cosine_sim(u=a_mean_rep, v=c_hidden) for c_hidden in c_hiddens])
+                        c_most_sim_idx = np.argmax(cos_sims_a_and_c) # similarity measure ==> argmax
+                        c_most_sim = c_hiddens[c_most_sim_idx]
+                        c_most_sim_mean_cos = np.mean([cosine_sim(u=c_most_sim, v=a_hidden) for a_hidden in a_hiddens])
+                        a_mean_cos = compute_ans_distances(a_hiddens, metric)
 
-                        if a_hiddens.shape[0] == 1:
-                            if normalization:
-                                q_hiddens -= hiddens_mu
-                                q_hiddens /= hiddens_sigma
-
-                            q_mean_rep = q_hiddens.mean(0)
-                            c_mean_rep = c_hiddens.mean(0)
-
-                            cos_sim_a_q = cosine_sim(u=q_mean_rep, v=a_mean_rep)
-                            cos_sim_a_c = cosine_sim(u=c_mean_rep, v=a_mean_rep)
-
-                            if cos_sim_a_q > cos_sim_a_c:
-                                est_preds_current_layer[k] += 1
-                        else:
-                            cos_sims_a_and_c = np.array([cosine_sim(u=a_mean_rep, v=c_hidden) for c_hidden in c_hiddens])
-                            c_most_sim_idx = np.argmax(cos_sims_a_and_c) # similarity measure ==> argmax
-                            c_most_sim = c_hiddens[c_most_sim_idx]
-                            c_most_sim_mean_cos = np.mean([cosine_sim(u=c_most_sim, v=a_hidden) for a_hidden in a_hiddens])
-                            a_mean_cos = compute_ans_distances(a_hiddens, metric)
-
-                            if a_mean_cos > c_most_sim_mean_cos: #np.max(cos_sims_a_and_c):
-                                est_preds_current_layer[k] += 1
+                        if a_mean_cos > c_most_sim_mean_cos: 
+                            est_preds_current_layer[k] += 1
 
                     elif metric == 'euclid':
 
-                        if a_hiddens.shape[0] == 1:
-                            if normalization:
-                                q_hiddens -= hiddens_mu
-                                q_hiddens /= hiddens_sigma
-
-                            q_mean_rep = q_hiddens.mean(0)
-                            c_mean_rep = c_hiddens.mean(0)
-
-                            dist_a_q = euclidean_dist(u=q_mean_rep, v=a_mean_rep)
-                            dist_a_c = euclidean_dist(u=c_mean_rep, v=a_mean_rep)
-
-                            if dist_a_q < dist_a_c:
-                                est_preds_current_layer[k] += 1
-
-                        else:
-                            euclid_dists_a_and_c = np.array([euclidean_dist(u=a_mean_rep, v=c_hidden) for c_hidden in c_hiddens])
-                            c_closest_idx = np.argmin(euclid_dists_a_and_c) # dissimilarity measure ==> argmin
-                            c_closest = c_hiddens[c_closest_idx]
-                            c_closest_mean_dist = np.mean([euclidean_dist(u=c_closest, v=a_hidden) for a_hidden in a_hiddens])
-                            a_mean_dist = compute_ans_distances(a_hiddens, metric)
-
-                            if a_mean_dist < c_closest_mean_dist: #np.min(euclid_dists_a_and_c):
-                                est_preds_current_layer[k] += 1
-
-                    elif metric == 'mahalanobis':
-                        if not normalization:
-                            hiddens = hiddens[sep_idx+1:-1, :]
-
-                        # compute (inv) cov mat w.r.t. both answer and context hidden reps
-                        try:
-                            inv_cov = np.linalg.inv(np.cov(hiddens.T))
-                        except:
-                            inv_cov = np.cov(hiddens.T)
-                        # compute (inv) cov mat w.r.t. answer hidden reps
-                        try:
-                            a_inv_cov = np.linalg.inv(np.cov(a_hiddens.T))
-                        except:
-                            a_inv_cov = np.cov(a_hiddens.T)
-
-                        mahalanob_dists_a_and_c = np.array([mahalanobis(u=a_mean_rep, v=c_hidden, VI=inv_cov) for c_hidden in c_hiddens])
-                        c_closest_idx = np.argmin(mahalanob_dists_a_and_c) # dissimilarity measure ==> argmin
+                        euclid_dists_a_and_c = np.array([euclidean_dist(u=a_mean_rep, v=c_hidden) for c_hidden in c_hiddens])
+                        c_closest_idx = np.argmin(euclid_dists_a_and_c) # dissimilarity measure ==> argmin
                         c_closest = c_hiddens[c_closest_idx]
+                        c_closest_mean_dist = np.mean([euclidean_dist(u=c_closest, v=a_hidden) for a_hidden in a_hiddens])
+                        a_mean_dist = compute_ans_distances(a_hiddens, metric)
 
-                        #TODO: figure out, whether we have to pass (inv) cov mat w.r.t. answer hidden reps or w.r.t. both answer and context hidden reps (...not sure about that)
-                        c_closest_mean_dist = np.mean([mahalanobis(u=c_closest, v=a_hidden, VI=inv_cov) for a_hidden in a_hiddens])
-                        a_mean_dist = compute_ans_distances(a_hiddens, metric, inv_cov=a_inv_cov)
-
-                        if a_mean_dist < c_closest_mean_dist: #np.min(mahalanob_dists_a_and_c)
+                        if a_mean_dist < c_closest_mean_dist:
                             est_preds_current_layer[k] += 1
                     k += 1
             est_preds_top_layers.append(est_preds_current_layer)
@@ -280,16 +293,23 @@ if __name__ == "__main__":
     args = parser.parse_args()
     # define variables
     source = args.source
-    metrics = ['cosine', 'euclid'] #, 'mahalanobis']
-    dims = ['high'] #['high', 'low']
+    metrics = ['cosine']#, 'euclid']
+    dims = ['low'] #['high', 'low']
     # get feat reps
     test_results, model_name = get_hidden_reps(source=source)
-    # estimate model predictions w.r.t. hidden reps in latent space (per transformer layer)
-    est_per_metric = {metric: {'norm' + '_'  + str(norm).lower(): evaluate_estimations(test_results, metric, 'high', norm) for norm in [True, False]} for metric in metrics}
-    #est_per_metric = {metric: {dim: evaluate_estimations(test_results, metric, dim) for dim in dims} for metric in metrics}
+    # estimate model predictions w.r.t. answer and context hidden reps in latent space (per transformer layer) AND
+    # compute (dis-)similarities among hidden representations in h_a for both correct and erroneous model predictions (at each layer)
+    ests_and_dists  = {metric: {dim: evaluate_estimations(test_results, metric, dim) for dim in dims} for metric in metrics}
+
+    hidden_reps_results = {}
+    hidden_reps_results['estimations'] = {metric: {dim: results[0] for dim, results in dims.items()} for metric, dims in ests_and_dists.items()}
+    hidden_reps_results['similarities'] = {metric: {dim: results[1] for dim, results in dims.items()} for metric, dims in ests_and_dists.items()}
+
     print()
-    print(est_per_metric)
+    print(hidden_reps_results)
     print()
+
     # save results
     with open('./results_hidden_reps/' + '/' + source.lower() + '/' + model_name + '.json', 'w') as json_file:
-        json.dump(est_per_metric, json_file)
+        json.dump(hidden_reps_results, json_file)
+
