b9750414d3ac55228718751ac2138dcf81b1d7dd
LukasMut <lukas-mt@live.at>
2020-05-06 10:50:28 +0200

    Update eval_hidden_reps.py

--- eval_hidden_reps.py
+++ eval_hidden_reps.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 __all__ = [
            'get_hidden_reps',
            'compute_ans_similarities',
@@ -759,7 +762,7 @@ if __name__ == "__main__":
         help='Set model save directory for ans prediction model. Only necessary if args.prediction == learned.')
     parser.add_argument('--batch_size', type=int, default=8,
         help='Specify mini-batch size. Only necessary if args.prediction == learned.')
-    parser.add_argument('--n_epochs', type=int, default=20,
+    parser.add_argument('--n_epochs', type=int, default=25,
         help='Set number of epochs model should be trained for. Only necessary if args.prediction == learned.')
     parser.add_argument('--layers', type=str, default='',
         help='Must be one of {all_layers, top_three_layers}. Only necessary if args.prediction == learned.')
