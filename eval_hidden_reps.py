66c697cf33f8927ebeadf28483672de0bc92de77
LukasMut <lukas-mt@live.at>
2020-05-13 19:06:43 +0200

    Update eval_hidden_reps.py

--- eval_hidden_reps.py
+++ eval_hidden_reps.py
@@ -914,9 +914,11 @@ if __name__ == "__main__":
     versions = ['train', 'test']
 
     if args.w_strategy == 'distance' and args.Layers == 'all_layers':
+        #we need to evaluate baseline approaches only once
         computations = ['baseline_concat', 'baseline_heuristic' 'raw', 'concat', 'weighting']
 
     elif args.w_strategy == 'distance' and args.Layers == 'top_three_layers':
+        #*raw* does not need to be evaluated for both weighting strategies
         computations = ['raw', 'concat', 'weighting']
     
     else:
