a75c33784ea6140e9ab8283def14e5d830be7e8b
LukasMut <lukas-mt@live.at>
2020-05-18 13:45:38 +0200

    increased number of epochs to train neural net for

--- eval_hidden_reps.py
+++ eval_hidden_reps.py
@@ -970,7 +970,7 @@ if __name__ == "__main__":
         help='Set model save directory for ans prediction model.')
     parser.add_argument('--batch_size', type=int, default=8,
         help='Specify mini-batch size.')
-    parser.add_argument('--n_epochs', type=int, default=20,
+    parser.add_argument('--n_epochs', type=int, default=25,
         help='Set number of epochs model should be trained for.')
     parser.add_argument('--layers', type=str, default='',
         help='Must be one of {all_layers, top_three_layers}.')
