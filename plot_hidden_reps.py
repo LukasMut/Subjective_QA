273a5900045b2769440b535384564a563d889bd4
LukasMut <lukas-mt@live.at>
2020-05-28 11:21:13 +0200

    added comments

--- plot_hidden_reps.py
+++ plot_hidden_reps.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import argparse
 import numpy as np
 import matplotlib as mpl 
--- plotting.py
+++ plotting.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 __all__ = [
            'conf_mat',
            'plot_confusion_matrix',
